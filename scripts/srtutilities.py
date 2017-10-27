121d4278f0b77f3bea074264d6d12aa43f575de3
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2017-10-27 17:21:42 +0000

    adding location objects

--- scripts/srtutilities.py
+++ scripts/srtutilities.py
@@ -37,4 +37,36 @@ def prep(orig,inputfile):
         allines = [[x for x in line.strip('\n').split(' ') if x != ''] for line in f.readlines()]
     return allines
 
+class locations(object):
 
+    def __init__(self):
+        self.locations = {'norman':['35d13m21.2s','-97d26m22.1s',370,-4],\
+                          'apo':['','',2300,-5]}
+
+    def get_locations(self):
+        return ', '.join([i for i in self.locations])
+
+    def add_location(self,location):
+        if not self.verify_location(location[0]):
+            try:
+                if len(location) == 5:
+                    self.locations[location[0]] = [location[1],location[2],float(location[3]),int(location[4])]
+                else:
+                    raise RuntimeError('Incorrect format, please follow: \n\
+                                        [{},{}] which is [name,lat,long,height,timezone] and \n\
+                                        lat is North bias, long is East Bias, height is in meters, timezone is integer from utc\
+                                        '.format('norman',','.join(self.locations['norman'])))
+            except IndexError:
+                raise RuntimeError('Incorrect format, please follow: \n\
+                                    [{},{}] which is [name,lat,long,height,timezone] and \n\
+                                    lat is North bias, long is East Bias,height is in meters, timezone is integer from utc\
+                                    '.format('norman',','.join(self.locations['norman'])))
+
+    def verify_location(self,checking):
+        if checking in self.locations:
+            return True
+        else:
+            return False
+
+if __name__ == "__main__":
+    print("Testing module...")
