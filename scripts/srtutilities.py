fa0522de9215004c9613856bf10a8e56248a6993
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2017-11-01 01:40:32 +0000

    adding useful functions

--- scripts/srtutilities.py
+++ scripts/srtutilities.py
@@ -8,6 +8,7 @@ Misc  : File will handle misc srt specific function
 
 # import modules
 from os import system as _SYSTEM_
+from sys import exit
 
 # creates example data file
 def example_data(logger):
@@ -67,6 +68,54 @@ class locations(object):
             return True
         else:
             return False
+
+class special(object):
+
+    def fwhm(self,sig):
+        import numpy as np
+        return 2. * ((2. * np.log10(2.)) ** 0.5) * (sig)
+
+    def get_params(self):
+        return self.params
+
+    def set_params(self,params):
+        self.params = params
+
+    # single gaussian
+    def gaussian(self,x,mu,sig,A):
+        from numpy import inner,exp
+        vector = inner(x - mu,x - mu)
+        return A * exp(-0.5 * (vector/sig)** 2)
+
+    # multigaussian single dimension
+    def multigauss(self,x,mugrid,siggrid,Agrid):
+        total = []
+        for mu,sig,A in zip(mugrid,siggrid,Agrid):
+            temp = self.gaussian(x,mu,sig,A)
+            total += temp
+        if len(total) != len(x):
+            raise RuntimeError('Shape of multigauss and x range not the same')
+            exit()
+        else:
+            return total
+
+    def polynomial(self,x,coeff):
+        total = []
+        if (type(coeff) != list) or (type(coeff) != np.ndarray):
+            coeff = [coeff,]
+        for order in range(len(coeff)):
+            temp = coeff[order] * x[:] ** order
+            total += temp
+        if len(total) != len(x):
+            raise RuntimeError('Shape of polynomial and x range not the same')
+            exit()
+        else:
+            return total
+
+    # bessel function
+    def bessel2(self,x ,order):
+        from scipy.special import yv
+        return yv(order, x)
 
 if __name__ == "__main__":
     print("Testing module...")
